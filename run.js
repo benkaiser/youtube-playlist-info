e17779b940a216d61622af2cfa0173d5e7321d57
FireController1847 <usfirepilot123@gmail.com>
2017-11-15 18:20:46 -0700

    Standardize Callback & Add Promise Support

--- run.js
+++ run.js
@@ -8,7 +8,7 @@ function playlistInfoRecursive(playlistId, callStackSize, pageToken, currentItem
     maxResults: 50,
     playlistId: playlistId,
   }, function(err, data) {
-    if (err) callback({error: err}); return;
+    if (err) callback(err); return;
 
     for (var x in data.items) {
       currentItems.push(data.items[x].snippet);
@@ -17,20 +17,28 @@ function playlistInfoRecursive(playlistId, callStackSize, pageToken, currentItem
     if (data.nextPageToken) {
       playlistInfoRecursive(playlistId, callStackSize + 1, data.nextPageToken, currentItems, callback);
     } else {
-      callback(currentItems);
+      callback(null, currentItems);
     }
 
   });
 }
 
-exports.playlistInfo = function(apiKey, playlistId, done) {
-
+exports.playlistInfo = function getPlaylistInfo(apiKey, playlistId, callback) {
+    if (!done) {
+    return new Promise(function(resolve, reject) {
+      getPlaylistInfo(apiKey, playlistId, function(err, info) {
+        if (err) return reject(err);
+        resolve(info);
+      });
+    });
+  }
+  
   youtube.authenticate({
     type: 'key',
     key: apiKey,
   });
 
-  playlistInfoRecursive(playlistId, 0, null, [], done);
+  playlistInfoRecursive(playlistId, 0, null, [], callback);
 };
 
 // pullPlayList("playlist id here", "name of mp3 here");
