2b3e0d72c52d319d0ec4941f6416f18a5beea900
FireController1847 <usfirepilot123@gmail.com>
2017-11-28 11:59:15 -0700

    Resolve merge conflict

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ Youtube Playlist Info is a library that fetches all the information for the song
 
 The library does not currently emit progress events, however pull requests are welcome.
 
-# Usage
+### Installation
+```sh
+npm install --save youtube-playlist-info
+```
+
+### Usage
 ```js
 const ypi = require('youtube-playlist-info');
 ypi("YouTube API Key", "Playlist ID").then(items => {
@@ -12,31 +17,11 @@ ypi("YouTube API Key", "Playlist ID").then(items => {
 }).catch(console.error);
 ```
 
-# Options
-
-- `maxResults` - The maxmimum amount of results to return from the playlist, starting from 0.
-
-### Options Example
-```js
-const ypi = require('youtube-playlist-info');
-const options = {
-  maxResults: 25
-};
-ypi("YouTube API Key", "Playlist ID", options).then(items => {
-  console.log(items);
-}).catch(console.error);
-```
-
-# Installation
-```sh
-npm install --save youtube-playlist-info
-```
-
-# Testing
+### Testing
 
 ```sh
 set API_KEY=YouTube API Key
-npm test
+node test.js
 ```
 
 This should just spit out a bunch of items in the playlist followed by the length of the playlist.
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 const youtube = require('youtube-api');
 const fs = require('fs');
 
-function playlistInfoRecursive(playlistId, callStackSize, pageToken, currentItems, customRequestAmount, callback) {
+function playlistInfoRecursive(playlistId, callStackSize, pageToken, currentItems, callback) {
   youtube.playlistItems.list({
     part: 'snippet',
     pageToken: pageToken,
-    maxResults: (customRequestAmount > 50 || !customRequestAmount ? 50 : customRequestAmount),
+    maxResults: 50,
     playlistId: playlistId,
   }, function(err, data) {
     if (err) return callback(err);
     for (const x in data.items) {
       currentItems.push(data.items[x].snippet);
     }
-    if (data.nextPageToken && (customRequestAmount > 50 || !customRequestAmount)) {
-      playlistInfoRecursive(playlistId, callStackSize + 1, data.nextPageToken, currentItems, (customRequestAmount > 50 ? customRequestAmount - 50 : customRequestAmount), callback);
+    if (data.nextPageToken) {
+      playlistInfoRecursive(playlistId, callStackSize + 1, data.nextPageToken, currentItems, callback);
     } else {
       callback(null, currentItems);
     }
   });
 }
 
-module.exports = function playlistInfo(apiKey, playlistId, {maxResults}) {
+module.exports = function playlistInfo(apiKey, playlistId) {
   return new Promise((resolve, reject) => {
     if (!apiKey) return reject(new Error('No API Key Provided'));
     if (!playlistId) return reject(new Error('No Playlist ID Provided'));
@@ -28,7 +28,7 @@ module.exports = function playlistInfo(apiKey, playlistId, {maxResults}) {
       type: 'key',
       key: apiKey
     });
-    playlistInfoRecursive(playlistId, 0, null, [], maxResults, (err, list) => {
+    playlistInfoRecursive(playlistId, 0, null, [], (err, list) => {
       if (err) return reject(err);
       return resolve(list);
     });
